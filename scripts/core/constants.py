05d39117cdbebd5e848d9b7a524d9aa0b1a6cda4
goy <yogarizky45@gmail.com>
2025-12-19 08:21:59 +0700

    Update constants.py

--- scripts/core/constants.py
+++ scripts/core/constants.py
@@ -27,7 +27,7 @@ CONFIG_DIR = "core/config/"
 OUTPUT_DIR = "core/outputs/"
 CACHE_DIR = "~/.cache/huggingface"
 CACHE_DIR_HUB = os.path.expanduser("~/.cache/huggingface/hub")
-DIFFUSION_DATASET_DIR = "core/dataset/images"
+DIFFUSION_DATASET_DIR = "/dataset/images"
 CONTAINER_FLUX_PATH = "/app/flux/unet"
 
 DIFFUSION_SDXL_REPEATS = 5
